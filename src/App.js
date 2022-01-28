60b65e2fbb89c483f28100a4ed0d61b4d4551ceb
Javi <javi@jsrois.net>
2022-01-28 15:23:23 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [counter, setCounter] = useState(0);
+    const [message, setMessage] = useState("");
+
+    useEffect(function(){
+
+        if (counter === 0) {
+            setMessage("Haz click para empezar a contar");
+            return;
+        }
+
+        if (counter % 3 === 0 && counter % 5 === 0) {
+            setMessage("FizzBuzz");
+        } else if (counter % 3 === 0) {
+            setMessage("Fizz");
+        } else if (counter % 5 === 0) {
+            setMessage("Buzz")
+        } else {
+            setMessage(counter);
+        }
+    }, [counter])
+
+    function incrementCounter() {
+        setCounter(counter+1);
+    }
+
+
+    return (
+        <div className="App">
+            <h2>Fizz Buzz</h2>
+            <button onClick={incrementCounter}>Click me!</button>
+            <div className="message">{message}</div>
+        </div>
+    );
 }
 
 export default App;
